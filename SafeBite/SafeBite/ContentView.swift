6abc20e74e9643f5e5106b1e1b60d3862f3a7a00
christopheradolphe <ckwadolphe@gmail.com>
2024-06-02 17:20:59 -0400

    Formatting for Restaurant cards

--- SafeBite/SafeBite/ContentView.swift
+++ SafeBite/SafeBite/ContentView.swift
@@ -26,19 +26,35 @@ struct ContentView: View {
                     VStack(alignment: .leading) {
                         Text("Asian")
                             .font(.title.bold())
+                            .padding([.horizontal, .top])
                         
                         ScrollView(.horizontal, showsIndicators: false) {
                             HStack {
                                 ForEach(restaurants) { restaurant in
                                     NavigationLink {
-                                        Text("Entering restaurant")
+                                        Text("Entering restaurant \(restaurant.name)")
                                     } label: {
                                         VStack {
                                             Image(restaurant.image)
                                                 .resizable()
                                                 .scaledToFit()
                                                 .frame(width: 100, height: 100)
+                                                .padding()
+                                            VStack {
+                                                Text(restaurant.name)
+                                                    .font(.subheadline)
+                                                    .foregroundStyle(.white)
+                                                    .frame(maxWidth: 100)
+                                            }
+                                            .frame(maxWidth: .infinity)
+                                            .background(.green)
                                         }
+                                        .clipShape(.rect(cornerRadius: 10))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 10)
+                                                .stroke(.green)
+                                        )
+                                        .padding(.horizontal, 10)
                                     }
                                 }
                             }
