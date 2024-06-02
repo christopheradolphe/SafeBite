e63c2667ef3e1d18b6bce95a46eaa89f7c9d176f
christopheradolphe <ckwadolphe@gmail.com>
2024-06-02 16:25:44 -0400

    Add Separator view code

--- SafeBite/SafeBite/ContentView.swift
+++ SafeBite/SafeBite/ContentView.swift
@@ -7,6 +7,15 @@
 
 import SwiftUI
 
+struct Separator: View {
+    var body: some View {
+         Rectangle()
+            .frame(height: 2)
+            .foregroundStyle(.black)
+            .padding(.vertical)
+    }
+}
+
 struct ContentView: View {
     let restaurants: [Restaurant] = Bundle.main.decode("restaurants.json")
     
