e2c6ca0ac21d88943b52f3383fd52e11b07db7bc
christopheradolphe <ckwadolphe@gmail.com>
2024-06-02 14:02:16 -0400

    Start of Swift Project

---
+++ SafeBite/SafeBite/SafeBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeBiteApp.swift
+//  SafeBite
+//
+//  Created by Christopher Adolphe on 2024-06-02.
+//
+
+import SwiftUI
+
+@main
+struct SafeBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
