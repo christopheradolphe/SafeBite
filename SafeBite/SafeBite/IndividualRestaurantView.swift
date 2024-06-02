75a670e7d6c4ac99b22105ddd43162701164cd01
christopheradolphe <ckwadolphe@gmail.com>
2024-06-02 17:56:44 -0400

    Add allergens for menu items

---
+++ SafeBite/SafeBite/IndividualRestaurantView.swift
@@ -0,0 +1,18 @@
+//
+//  IndividualRestaurantView.swift
+//  SafeBite
+//
+//  Created by Christopher Adolphe on 2024-06-02.
+//
+
+import SwiftUI
+
+struct IndividualRestaurantView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IndividualRestaurantView()
+}
--- SafeBite/SafeBite/Menu.swift
+++ SafeBite/SafeBite/Menu.swift
@@ -11,9 +11,25 @@ struct MenuItem: Codable, Identifiable {
     let id: Int
     let name: String
     let itemType: String
-    let allergens: [String]
     let image: String //maybe make this computed
     let description: String
+    
+    //Need to find better solution for allergens
+    let gluten: Int?
+    let sesame: Int?
+    let soy: Int?
+    let fish: Int?
+    let shellfish: Int?
+    let dairy: Int?
+    let egg: Int?
+    let treeNuts: Int?
+    let peanuts: Int?
+    let wheat: Int?
+    let almonds: Int?
+    let legumes: Int?
+    let mustard: Int?
+    let vegetarian: Int?
+    let vegan: Int?
 }
 
 struct Menu: Codable {
