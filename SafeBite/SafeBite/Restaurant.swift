64507a3bb4dff25f3646365a1b5cc7d3a5da695f
christopheradolphe <ckwadolphe@gmail.com>
2024-06-02 14:27:12 -0400

    Add enum for cuisine type

--- SafeBite/SafeBite/Restaurant.swift
+++ SafeBite/SafeBite/Restaurant.swift
@@ -11,8 +11,12 @@ struct Restaurant: Codable, Identifiable {
     let id: String
     let name: String
     let description: String
-    let cuisine: String //Make this an enum eventually
+    let cuisine: Cuisine //Make this an enum eventually
     let address: [String]
     let phoneNumber: String
     let website: String
+}
+
+enum Cuisine: Codable {
+    case Asian, Italian, Tapas, Steakhouse, BarAndGrill, Mexican
 }
