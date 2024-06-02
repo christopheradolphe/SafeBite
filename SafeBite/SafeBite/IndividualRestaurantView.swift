83e66e98290c2ee317375c2836b0c15b7ebe9197
christopheradolphe <ckwadolphe@gmail.com>
2024-06-02 18:22:45 -0400

    Initializer for IndividualRestaurantView to load in json file

--- SafeBite/SafeBite/IndividualRestaurantView.swift
+++ SafeBite/SafeBite/IndividualRestaurantView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct IndividualRestaurantView: View {
+    let menu: Menu
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    init(restaurant: String) {
+        let filename = restaurant + ".json"
+        self.menu = Menu(menuItems: Bundle.main.decode(filename))
     }
 }
 
 #Preview {
-    IndividualRestaurantView()
+    IndividualRestaurantView(restaurant: "missbao")
 }
