d17d505a0e282d06cfe996792e43c4fc3668f665
christopheradolphe <ckwadolphe@gmail.com>
2024-06-02 19:32:02 -0400

    Add list of menu items to restaurant view

--- SafeBite/SafeBite/IndividualRestaurantView.swift
+++ SafeBite/SafeBite/IndividualRestaurantView.swift
@@ -10,31 +10,31 @@ import SwiftUI
 struct IndividualRestaurantView: View {
     let restaurtant: Restaurant
     let menu: Menu
-    
+    let menuTypes = ["SMALL PLATES", "DESSERT", "BAO", "LARGE PLATES"]
+        
     var body: some View {
         NavigationStack {
-            ScrollView {
-                VStack(alignment: .leading) {
-                    Text("About \(restaurtant.name)")
-                        .font(.headline)
-                        .padding(.vertical, 10)
-                    Text(restaurtant.description)
-                        .font(.caption)
-                        .foregroundStyle(.gray)
-                }
-                .padding(.horizontal)
-                .navigationTitle(restaurtant.name)
-                .navigationBarTitleDisplayMode(.inline)
+            VStack(alignment: .leading) {
+                Text("About \(restaurtant.name)")
+                    .font(.headline)
+                    .padding(.vertical, 10)
+                Text(restaurtant.description) //Work to shorten this or make an info button to view
+                    .font(.caption)
+                    .foregroundStyle(.gray)
                 
-                VStack {
-                    ForEach(menu.menuItems) { menuItem in
-                        NavigationLink(menuItem.name)
-                    }
-                    .navigationDestination(for: MenuItem.self) { selection in
-                        Text("You just selected \(selection.name)")
-                    }
-                }
+                Separator()
             }
+            .padding(.horizontal)
+            
+            List(menu.menuItems) { menuItem in
+                NavigationLink(menuItem.name, value: menuItem)
+            }
+            .navigationDestination(for: MenuItem.self) { selection in
+                Text("You just selected \(selection.name)")
+            }
+        .navigationTitle(restaurtant.name)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbarBackground(.green)
         }
     }
     
