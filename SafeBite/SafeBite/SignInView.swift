74a2f5b0abc2d53743b212a1a5eb7cdfd033b391
christopheradolphe <ckwadolphe@gmail.com>
2024-06-12 00:11:28 -0400

    Add allergy section to SafeBite profile

--- SafeBite/SafeBite/SignInView.swift
+++ SafeBite/SafeBite/SignInView.swift
@@ -15,6 +15,9 @@ struct SignInView: View {
     
     @State private var vegetarian = false
     @State private var vegan = false
+    
+    @State private var peanuts = false
+    @State private var sesame = false
     var body: some View {
         NavigationStack {
             Form {
@@ -25,9 +28,14 @@ struct SignInView: View {
                     TextField("Phone Number", text: $phoneNumber)
                 }
                 
-                Section ("Allergies/Dietary Restrictions") {
+                Section ("Dietary Restrictions") {
                     Toggle("Vegetarian", isOn: $vegetarian)
                     Toggle("Vegan", isOn: $vegan)
+                }
+                
+                Section ("Allergies") {
+                    Toggle("Peanuts", isOn: $peanuts)
+                    Toggle("Sesame", isOn: $sesame)
                 }
             }
             .navigationTitle("SafeBite Profile")
