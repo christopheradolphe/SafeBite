625a3aecf140fe5c30c8c22011f809ee0d680042
christopheradolphe <ckwadolphe@gmail.com>
2024-06-11 23:53:20 -0400

    Create sign in view swift file

---
+++ SafeBite/SafeBite/SignInView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInView.swift
+//  SafeBite
+//
+//  Created by Christopher Adolphe on 2024-06-11.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInView()
+}
