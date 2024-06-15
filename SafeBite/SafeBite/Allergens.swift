7f1e7aeeb98fa52f15a2f550d49b2ef703a3c1f7
christopheradolphe <ckwadolphe@gmail.com>
2024-06-15 01:25:59 -0400

    Creation of Allergen and Dietary Restriction structs for user profile

---
+++ SafeBite/SafeBite/Allergens.swift
@@ -0,0 +1,34 @@
+//
+//  Allergens.swift
+//  SafeBite
+//
+//  Created by Christopher Adolphe on 2024-06-15.
+//
+
+import Foundation
+
+struct Allergens {
+    var Gluten = false
+    var Wheat = false
+    var Soy = false
+    var Shellfish = false
+    var Fish = false
+    var Dairy = false
+    var Egg = false
+    var TreeNuts = false
+    var Peanuts = false
+    var Sesame = false
+    var Mustard = false
+    var Garlic = false
+    var Sulfites = false
+}
+
+struct DietaryRestrictions {
+    var Vegan = false
+    var Vegetarian = false
+    var Halal = false
+    var Keto = false
+    var LowCarb = false
+    var LowFODMAP = false
+    var DashDiet = false
+}
