5b8b9a7539761d7a4ab666cac194ba21ca8b4358
christopheradolphe <ckwadolphe@gmail.com>
2024-07-26 20:39:40 -0400

    Move location of mapview struct

--- SafeBite/SafeBite/IndividualRestaurantView.swift
+++ SafeBite/SafeBite/IndividualRestaurantView.swift
@@ -8,37 +8,6 @@
 import SwiftUI
 import MapKit
 
-struct MapView: UIViewRepresentable {
-    let address: String
-
-    // Create and configure the map view
-    func makeUIView(context: Context) -> MKMapView {
-        MKMapView()
-    }
-
-    // Update the map view when needed
-    func updateUIView(_ mapView: MKMapView, context: Context) {
-        // Perform geocoding to convert address to coordinates
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(address) { (placemarks, error) in
-            guard let placemark = placemarks?.first, let location = placemark.location else {
-                // Handle error or display default location if geocoding fails
-                return
-            }
-            
-            // Add a pin to the map for the location
-            let annotation = MKPointAnnotation()
-            annotation.coordinate = location.coordinate
-            annotation.title = placemark.name
-            mapView.addAnnotation(annotation)
-            
-            // Set region to show the location on the map
-            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
-            mapView.setRegion(region, animated: true)
-        }
-    }
-}
-
 
 func imageExists(named name: String) -> Bool {
     return UIImage(named: name) != nil
